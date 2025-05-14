8303db2aaa902f514daacb978360b8327792d1dd
Aquiles Alves <aquilespereira789@gmail.com>
2025-05-14 19:43:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TROCA DE PLANO
+
---
+++ py.py
@@ -0,0 +1,20 @@
+vencimento_atual = int(input('Digite o vencimento atual: '))
+vencimento_novo = int(input('Digite o novo vencimento: '))
+valor_plano = float(input('Digite o valor atual do plano: '))
+
+if(vencimento_atual < vencimento_novo):
+    dias_proporcionais = vencimento_novo - vencimento_atual
+else:
+    dias_proporcionais = (30 - vencimento_atual) + vencimento_novo
+
+diaria_plano = valor_plano / 30
+proporcional = diaria_plano * dias_proporcionais
+valor_total = proporcional + valor_plano
+print(valor_total)
+
+
+valorAntigo = float(input('Digite o valor do plano antigo: '))
+valorNovo = float(input('Digite o valor do plano novo: '))
+vencimento = int(input('Digite a data do vencimento: '))
+dataTroca = int(input('Digite a data da troca: '))
+
